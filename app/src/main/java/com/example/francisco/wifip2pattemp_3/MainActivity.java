e5496c1831f0a70c84f80a329b6a5ccf4e515aa6
fbeltranmillalen <38946178+fbeltranmillalen@users.noreply.github.com>
2018-05-24 18:56:07 -0400

    Almost there :O

--- app/src/main/java/com/example/francisco/wifip2pattemp_3/MainActivity.java
+++ app/src/main/java/com/example/francisco/wifip2pattemp_3/MainActivity.java
@@ -1,23 +1,61 @@
 package com.example.francisco.wifip2pattemp_3;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.IntentFilter;
 import android.net.wifi.WifiManager;
+import android.net.wifi.p2p.WifiP2pConfig;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pDeviceList;
+import android.net.wifi.p2p.WifiP2pInfo;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final String TAG = getClass().getName();
+
+    //Objetos de interfaz
     Button btnOnOff, btnDiscover, btnSend;
     ListView listView;
     TextView read_msg_box, connectionStatus;
     EditText writeMsg;
 
+    //Objeto administrador de Wifi
     WifiManager wifiManager;
+
+    //Objetos administradores de Conexiones P2P
+    WifiP2pManager mManager;
+    WifiP2pManager.Channel mChannel;
+
+    //Objetos para unir el bradcastReceiver con esta clase, creo...
+    BroadcastReceiver mReceiver;
+    IntentFilter mIntentFilter;
+
+
+    //para peers conections
+
+    List<WifiP2pDevice> peers = new ArrayList<WifiP2pDevice>();
+    String[] deviceNameArray;
+    WifiP2pDevice[] deviceArray;
 
 
     @Override
@@ -28,7 +66,8 @@ public class MainActivity extends AppCompatActivity {
         exqListener();
     }
 
-    private void initialWork() {
+    private void initialWork() {//Metodo que inicializa los atributos de la clase
+        //Une los objetos declarados en la clase con los elementos gráficos de la clase
         btnOnOff = (Button) findViewById(R.id.onOff);
         btnDiscover = (Button) findViewById(R.id.discover);
         btnSend = (Button) findViewById(R.id.sendButton);
@@ -36,17 +75,77 @@ public class MainActivity extends AppCompatActivity {
         read_msg_box = (TextView) findViewById(R.id.readMsg);
         connectionStatus = (TextView) findViewById(R.id.connectionStatus);
         writeMsg = (EditText) findViewById(R.id.writeMsg);
+
+        //Inicializa objeto wifi para administrar conexiones wifi
         wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+        //Inicializa objeto administrador p2p
+        mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        mChannel = mManager.initialize(this, getMainLooper(), null);
+
+        //Inicializa los objetos necesarios para el broadCastReceiver
+        mReceiver = new WifiDirectBroadcastReceiver(mManager, mChannel, this);//Instancio mi clase WifiDirectBroadCastReceiver y accedo a los metdos de clase con mReceiver
+
+        //Mensajero que lleva 4 estados de wifiP2P a la clase BroadCastReceiver
+        mIntentFilter = new IntentFilter();
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
 
     }
 
-    private void exqListener() {
+    WifiP2pManager.PeerListListener peerListListener = new WifiP2pManager.PeerListListener() {//Esto no es un método, es una creación de objeto compleja
+        @Override
+        public void onPeersAvailable(WifiP2pDeviceList peerList) {
+            if(!peerList.getDeviceList().equals(peers)){
+                peers.clear();
+                peers.addAll(peerList.getDeviceList());
 
+                deviceNameArray = new String[peerList.getDeviceList().size()];
+                deviceArray = new WifiP2pDevice[peerList.getDeviceList().size()];
+                int index = 0;
+
+                for(WifiP2pDevice device: peerList.getDeviceList()){
+                    deviceNameArray[index] = device.deviceName;
+                    deviceArray[index] = device;
+                    index++;
+                }
+
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, deviceNameArray);
+                listView.setAdapter(adapter);
+            }
+            if(peers.size() == 0){
+                Toast.makeText(getApplicationContext(), "No device found", Toast.LENGTH_SHORT).show();
+                return;
+            }
+        }
+    };
+
+    WifiP2pManager.ConnectionInfoListener connectionInfoListener = new WifiP2pManager.ConnectionInfoListener() {
+        @Override
+        public void onConnectionInfoAvailable(WifiP2pInfo info) {
+            final InetAddress groupOwnerAddress = info.groupOwnerAddress;
+
+            if(info.groupFormed && info.isGroupOwner){
+                connectionStatus.setText("Host");
+            }
+            else if(info.groupFormed){
+                connectionStatus.setText("Client");
+            }
+
+        }
+    };
+
+    private void exqListener() {
         btnOnOff.setOnClickListener(new View.OnClickListener(){//unir boton con listener
             @Override
             public void onClick(View v) {
+
                 if(wifiManager.isWifiEnabled()){
                     wifiManager.setWifiEnabled(false);
+
                     btnOnOff.setText("ON");
 
                 }
@@ -57,5 +156,92 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        btnDiscover.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mManager.discoverPeers(mChannel, new WifiP2pManager.ActionListener() {
+                    @Override
+                    public void onSuccess() {
+                        connectionStatus.setText("Discovery Started");
+                    }
+
+                    @Override
+                    public void onFailure(int reason) {
+                        connectionStatus.setText("Discovery Starting Failure");
+
+                    }
+                });
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                final WifiP2pDevice device = deviceArray[position];
+                WifiP2pConfig config = new WifiP2pConfig();
+                config.deviceAddress = device.deviceAddress;
+
+                mManager.connect(mChannel, config, new WifiP2pManager.ActionListener() {
+                    @Override
+                    public void onSuccess() {
+                        Toast.makeText(getApplicationContext(), "Connected to " +device.deviceName+ "", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onFailure(int reason) {
+                        Toast.makeText(getApplicationContext(), "Not Connected", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(mReceiver, mIntentFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(mReceiver);
+    }
+
+    public class ServerClass extends  Thread{
+        Socket socket;
+        ServerSocket serverSocket;
+
+        @Override
+        public void run(){
+            try {
+                serverSocket = new ServerSocket(8888);
+                socket = serverSocket.accept();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    public class ClientClass extends  Thread{
+        Socket socket;
+        String hostAdd;
+
+        public ClientClass(InetAddress hostAddress){
+            hostAdd = hostAddress.getHostAddress();
+            socket = new Socket();
+        }
+
+        @Override
+        public void run() {
+            try {
+                socket.connect(new InetSocketAddress(hostAdd, 8888), 500);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
